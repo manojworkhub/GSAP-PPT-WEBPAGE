9a76e645d39f61a7755dd09cdabd8c08852f6e54
manojworkhub <155710796+manojworkhub@users.noreply.github.com>
2024-02-08 23:18:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+
+
+function myFunction(){ 
+
+let span1=document.querySelector(".sapn1")
+span1.style.display="inline-block"
+let span2=document.querySelector(".sapn2")
+span2.style.display="inline-block"
+let span3=document.querySelector(".sapn3")
+span3.style.display="inline-block"
+let sapn4=document.querySelector(".sapn4")
+sapn4.style.display="inline-block"
+}
